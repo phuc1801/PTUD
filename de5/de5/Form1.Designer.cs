7c0633c65d3d9353e23bb818d7df63645ef2b3d1
phuc <phuc180103@gmail.com>
2024-01-22 14:51:09 +0700

    de-5

---
+++ de5/de5/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace de5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaHH = new System.Windows.Forms.TextBox();
+            this.txtTenHH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.dgvHienThi = new System.Windows.Forms.DataGridView();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.txtGhiChu = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHienThi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMaHH
+            // 
+            this.txtMaHH.Location = new System.Drawing.Point(117, 62);
+            this.txtMaHH.Name = "txtMaHH";
+            this.txtMaHH.Size = new System.Drawing.Size(168, 20);
+            this.txtMaHH.TabIndex = 0;
+            // 
+            // txtTenHH
+            // 
+            this.txtTenHH.Location = new System.Drawing.Point(117, 111);
+            this.txtTenHH.Name = "txtTenHH";
+            this.txtTenHH.Size = new System.Drawing.Size(168, 20);
+            this.txtTenHH.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã HH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên HH";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(91, 221);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // dgvHienThi
+            // 
+            this.dgvHienThi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHienThi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHienThi.Location = new System.Drawing.Point(33, 276);
+            this.dgvHienThi.Name = "dgvHienThi";
+            this.dgvHienThi.Size = new System.Drawing.Size(682, 150);
+            this.dgvHienThi.TabIndex = 3;
+            this.dgvHienThi.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHienThi_CellClick);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(384, 62);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(168, 20);
+            this.txtTimKiem.TabIndex = 0;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(603, 61);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 2;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(308, 221);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(528, 221);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // txtGhiChu
+            // 
+            this.txtGhiChu.Location = new System.Drawing.Point(117, 157);
+            this.txtGhiChu.Name = "txtGhiChu";
+            this.txtGhiChu.Size = new System.Drawing.Size(168, 20);
+            this.txtGhiChu.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ghi chú";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 438);
+            this.Controls.Add(this.dgvHienThi);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGhiChu);
+            this.Controls.Add(this.txtTenHH);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.txtMaHH);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHienThi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMaHH;
+        private System.Windows.Forms.TextBox txtTenHH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.DataGridView dgvHienThi;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.TextBox txtGhiChu;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ de5/de5/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace de5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadHienThi();
+        }
+        string str = @"Data Source=DESKTOP-5NL0O53;Initial Catalog=K;Integrated Security=True";
+        SqlDataAdapter da;
+        DataTable dt;
+        void loadHienThi()
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(str))
+                {
+                    conn.Open();
+                    string query = "select MaHangHoa, TenHangHoa, GhiChu from HangHoa";
+                    da = new SqlDataAdapter(query, conn);
+                    dt = new DataTable();
+                    da.Fill(dt);
+                    dgvHienThi.DataSource = dt;
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dgvHienThi_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >=0 && e.RowIndex < dgvHienThi.Rows.Count)
+            {
+                int i = dgvHienThi.CurrentRow.Index;
+                txtMaHH.Text = dgvHienThi.Rows[i].Cells[0].Value.ToString();
+                txtTenHH.Text = dgvHienThi.Rows[i].Cells[1].Value.ToString();               
+                txtGhiChu.Text = dgvHienThi.Rows[i].Cells[2].Value.ToString();
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (batLoi())
+                {
+                    using (SqlConnection conn = new SqlConnection(str))
+                    {
+                        conn.Open();
+                        string query = "insert HangHoa(TenHangHoa, GhiChu) values (@TenHH, @GhiChu)";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@TenHH", txtTenHH.Text);
+                            cmd.Parameters.AddWithValue("@GhiChu", txtGhiChu.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+                        loadHienThi();
+                    }
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (batLoi())
+                {
+                    using (SqlConnection conn = new SqlConnection(str))
+                    {
+                        conn.Open();
+                        string query = "update HangHoa set TenHangHoa = @TenHH, GhiChu = @GhiChu where MaHangHoa = @MaHH";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@MaHH", int.Parse(txtMaHH.Text));
+                            cmd.Parameters.AddWithValue("@TenHH", txtTenHH.Text);
+                            cmd.Parameters.AddWithValue("@GhiChu", txtGhiChu.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+                        loadHienThi();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(str))
+                {
+                    conn.Open();
+                    string query = "delete from HangHoa where MaHangHoa = @MaHH";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@MaHH", int.Parse(txtMaHH.Text));                        
+                        cmd.ExecuteNonQuery();
+                    }
+                    loadHienThi();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        bool batLoi()
+        {
+            if(txtTenHH.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Ten khong duoc de trong");
+                return false;
+            }
+            if (txtTenHH.Text.Trim().Length > 30)
+            {
+                MessageBox.Show("Ten khong duoc vuot qua 30 ky tu");
+                return false;
+            }
+            if(int.TryParse(txtTenHH.Text, out int ten))
+            {
+                MessageBox.Show("Ten khong duoc la so");
+                return false;
+            }
+            if (txtGhiChu.Text.Trim().Length > 100)
+            {
+                MessageBox.Show("Ghi khong duoc vuot qua 100 ky tu");
+                return false;
+            }
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                string query = "select count(*) from HangHoa where MaHangHoa = @MaHH";
+               if(int.TryParse(txtMaHH.Text, out int ma))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@MaHH", ma);
+                        int count = (int)cmd.ExecuteScalar();
+                        if (count > 0)
+                        {
+                            MessageBox.Show("MaHH va Ten HH da co trong CSDL");
+                            return false;
+                        }
+                    }
+                }
+                
+            }
+
+            return true;
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtTimKiem.Text.Trim().Length == 0)
+                {
+                    loadHienThi();
+                }
+                else
+                {
+                    if(int.TryParse(txtTimKiem.Text, out int ma)){
+                        using (SqlConnection conn = new SqlConnection(str))
+                        {
+                            conn.Open();
+                            string query = "select MaHangHoa, TenHangHoa, GhiChu from HangHoa where MaHangHoa = @MaHH";
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@MaHH", ma);
+                                da = new SqlDataAdapter(cmd);
+                                dt = new DataTable();
+                                da.Fill(dt);
+                                dgvHienThi.DataSource = dt;
+                            }
+                            
+                        }
+                    }
+                    else
+                    {
+                        using (SqlConnection conn = new SqlConnection(str))
+                        {
+                            conn.Open();
+                            string query = "select MaHangHoa, TenHangHoa, GhiChu from HangHoa where TenHangHoa like '%'+@TenHH + '%'";
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@TenHH", txtTimKiem.Text);
+                                da = new SqlDataAdapter(cmd);
+                                dt = new DataTable();
+                                da.Fill(dt);
+                                dgvHienThi.DataSource = dt;
+                            }
+
+                        }
+                    }
+                }
+            }catch(Exception ex) { 
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
